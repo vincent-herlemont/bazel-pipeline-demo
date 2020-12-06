b98324279e71dd4171e8f46e0591732d2b6c7bd4
Vincent Herlemont <vincentherl@leszeros.com>
2020-12-06 17:14:38 +0000

    clean dispatcher

--- dispatcher/dispatcher.go
+++ dispatcher/dispatcher.go
@@ -5,47 +5,9 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 	"strconv"
-
 	"github.com/streadway/amqp"
 )
-
-func Sum(a int, b int) int {
-	return a + b
-}
-
-func testMq() {
-	conStr := fmt.Sprintf("amqp://guest:guest@%s:5672/", os.Getenv("AMQP_HOST"))
-	conn, err := amqp.Dial(conStr)
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer conn.Close()
-
-	ch, err := conn.Channel()
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer ch.Close()
-
-	q, err := ch.QueueDeclare("data1", false, false, false, false, nil)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	ch.Publish("", q.Name, false, false, amqp.Publishing{
-		ContentType: "text/plain",
-		Body:        []byte("Hello World !"),
-	})
-
-	msgs, err := ch.Consume(q.Name, "", true, false, false, false, nil)
-
-	for d := range msgs {
-		fmt.Println("consume message : ", string(d.Body))
-		break
-	}
-}
 
 func Handler(c chan int) func(w http.ResponseWriter, r *http.Request)  {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -73,20 +35,20 @@ func main() {
 	fmt.Println("url amqp: ",url)
 	conn, err := amqp.Dial(amqpCfg.url())
 	if err != nil {
-		fmt.Println(err)
+		log.Fatalln(err)
 	}
 	defer conn.Close()
 
 	ch, err := conn.Channel()
 	if err != nil {
-		fmt.Println(err)
+		log.Fatalln(err)
 	}
 	defer ch.Close()
 
 
 	q, err := ch.QueueDeclare("data", false, false, false, false, nil)
 	if err != nil {
-		fmt.Println(err)
+		log.Fatalln(err)
 	}
 
 	c := make(chan int)
--- dispatcher/dispatcher_test.go
+++ dispatcher/dispatcher_test.go
@@ -8,14 +8,6 @@ import (
 	"testing"
 )
 
-func TestSum(t *testing.T) {
-	total := Sum(5, 5)
-	if total != 10 {
-		t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
-	}
-}
-
-
 func TestHandler(t *testing.T) {
 	c := make(chan int,1)
 	sData := strconv.Itoa(10)
